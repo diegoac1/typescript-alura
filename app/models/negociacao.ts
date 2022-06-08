1e3115b1bdb314cbdb6a09e6b9e33bfcb4cc5c41
Diego <diegoacosta005@gmail.com>
2022-06-08 08:28:05 -0300

    first commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,25 @@
+export class Negociacao {
+
+    constructor(
+        public readonly _data: Date, 
+        public readonly quantidade: number,
+        public readonly valor: number
+    ) {}
+
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
+    get data(): Date {
+        return new Date(this._data.getTime());
+    }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+
+        return new Negociacao(date, quantidade,valor);
+    }
+}
